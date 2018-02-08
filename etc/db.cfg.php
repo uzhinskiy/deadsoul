e1af7fe7907b9d3cb5db53035aea9f684bf9fd22
Uzhinskiy Boris <lspci@mail.ru>
2018-02-09 01:11:30 +0400

    .

---
+++ etc/db.cfg.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Конфигурационный файл
+* @package	deadsoul
+* @author	Ужинский Борис email - boris.ujinsky@gmail.com
+* @file		db.cfg.php
+* @version	0.0.4
+* @filesource
+*/
+
+define ("DB_USER", "user");         # DB user name
+define ("DB_PASS", "pass");         # DB user password
+define ("DB_NAME", "db");           # DB name
+define ("DB_HOST", "localhost");    # DB host
+define ("DB_TYPE", "mysql");        # DB type (mysql/pgsql)
+
+define ("MEM_PORT", "11211");       # memcache port
+define ("MEM_HOST", "localhost");   # memcache host
+?>
