71e579469fe09003258297a7f15b5df22a83ac64
Uzhinskiy Boris <lspci@mail.ru>
2018-02-09 01:21:22 +0400

    update init.php

--- etc/init.php
+++ etc/init.php
@@ -5,22 +5,13 @@ function init(){
     global $MODULES;
     $libs = scandir(ROOT.LIBPHP);
     foreach ($libs as $lib) {
-		if ($lib === '.' || $lib === '..' || $lib === 'sql' || $lib === 'nosql' || $lib === 'classes')  continue;
-		if ( preg_match("/~/", $lib) ) continue;
-		    $MODULES[substr($lib, 4, -4)] = require_once(ROOT.LIBPHP."/".$lib);
+	if ($lib === '.' || $lib === '..' || $lib === 'sql' || $lib === 'nosql' || $lib === 'classes')  continue;
+	if ( preg_match("/~/", $lib) ) continue;
+	    $MODULES[substr($lib, 4, -4)] = require_once(ROOT.LIBPHP."/".$lib);
     }
     $MODULES[DB_TYPE] = require_once(ROOT.LIBPHP."/sql/lib_".DB_TYPE.".php");
 }
 
 init();
 session_start();
-set_locale();
-
-$registry = new Registry;
-
-$cm = new Cache(PACKAGE);
-$cm->connect(MEM_HOST, MEM_PORT);
-
-$registry->set ('CM', $cm); unset($cm);
-$registry->set("SEF",1);
 ?>
